4bbd563db4d45d21222ead68ae5bea72273c097a
frolofpavel <wwwfrolof@yandex.ru>
2026-04-28 23:32:45 +0700

    fix: rewrite /kp/:slug → static index.html

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/kp/:slug",
+        destination: "/kp/:slug/index.html",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
